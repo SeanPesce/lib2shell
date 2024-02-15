3ab2c0e7bb378028f9ebb487980a9f1e8c33d2a2
Sean Pesce <pescesean@gmail.com>
2024-02-15 10:53:14 -0500

    Update lib2shell.c
    Separate shell path and subsequently-executed shell command

--- unix/lib2shell.c
+++ unix/lib2shell.c
@@ -3,12 +3,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#define SHELL_PATH "/bin/sh"
 #define SHELL_COMMAND "/bin/sh"
  
 void __attribute__ ((constructor)) constructor()
 {
     puts("[lib2shell by SeanP]");
     printf("Starting %s\n", SHELL_COMMAND);
-    long long err = execl(SHELL_COMMAND, "/bin/sh", "-c", SHELL_COMMAND, NULL);
+    long long err = execl(SHELL_PATH, SHELL_PATH, "-c", SHELL_COMMAND, NULL);
     printf("Result: %lld\n", err);
 }
